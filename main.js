17f501c93080a785f41225d5562bed236b2bcd75
Keisy <keisy.nf@gmail.com>
2024-04-12 21:45:10 -0600

    Update canvas width in index.html and refactor square drawing logic in main.js

--- main.js
+++ main.js
@@ -1,46 +1,43 @@
-// // Canvas variables
-// const canvas = document.getElementById("myCanvas");
-// const ctx = canvas.getContext("2d");
-// const rect = canvas.getBoundingClientRect();
+// Canvas variables
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
 
-// // Keyboard functions
-// function keyDownHandler(e) {
-//   if (e.key === "Right" || e.key === "ArrowRight") {
-//     rightPressed = true;
-//   } else if (e.key === "Left" || e.key === "ArrowLeft") {
-//     leftPressed = true;
-//   }
-// }
+// Square Variables
+let squaresRowCount = 10;
+let size = canvas.width / squaresRowCount;
+let x = 0;
+let y = 0;
+const squareColor1 = "#ffceda";
+const squareColor2 = "#febbcf";
 
-// function keyUpHandler(e) {
-//   if (e.key === "Right" || e.key === "ArrowRight") {
-//     rightPressed = false;
-//   } else if (e.key === "Left" || e.key === "ArrowLeft") {
-//     leftPressed = false;
-//   }
-// }
+class Squares {
+  constructor(x, y, size, color) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.color = color;
+  }
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.rect(this.x, this.y, this.size, this.size);
+    ctx.fill();
+    ctx.closePath();
+  }
+}
 
-// function touchMoveHandler(e) {
-//   const relativeX = e.touches[0].clientX - canvas.offsetLeft;
-//   if (relativeX > 0 && relativeX < canvas.width) {
-//     paddle.x = relativeX - paddle.width / 2;
-//   }
-// }
-
-// class Brick {
-//   constructor(x, y, status, color) {
-//     this.x = x;
-//     this.y = y;
-//     this.status = status;
-//     this.color = color;
-//   }
-//   draw() {
-//     if (this.status === 1) {
-//       ctx.beginPath();
-//       ctx.rect(this.x, this.y, brickWidth, brickHeight);
-//       ctx.fillStyle = this.color;
-//       ctx.fill();
-//       ctx.closePath();
-//     }
-//   }
-// }
+for (let i = 0; i < squaresRowCount; i++) {
+  for (let j = 0; j < squaresRowCount; j++) {
+    if ((i % 2 === 0 && j % 2 === 0) || (i % 2 !== 0 && j % 2 !== 0)) {
+      let square = new Squares(x, y, size, squareColor1);
+      square.draw();
+      x += size;
+    } else {
+      let square = new Squares(x, y, size, squareColor2);
+      square.draw();
+      x += size;
+    }
+  }
+  x = 0;
+  y += size;
+}
