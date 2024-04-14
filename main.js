eb5780721f40ff061d8aa6733d3581a1487c4643
Keisy <keisy.nf@gmail.com>
2024-04-14 15:00:39 -0600

    Update snake movement and add snake color in main.js

--- main.js
+++ main.js
@@ -5,146 +5,103 @@ const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
 // Square Variables
-let squaresRowCount = 10;
+let squaresRowCount = 15;
 let size = canvas.width / squaresRowCount;
 let x = 0;
 let y = 0;
 const squareColor1 = "#ffceda";
 const squareColor2 = "#febbcf";
+const snakeColor = "#bde0fe";
 
 // Snake Variables
-let rightPressed = false;
-let leftPressed = false;
-let upPressed = false;
-let downPressed = false;
-
-// FPS Variables
-let fps = 1; // Desired fps
-let now;
-let then = Date.now();
-let interval = 1000 / fps;
-let delta;
-
-//* Event listeners
-
-// Keyboard event listeners
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
-
-//* Functions
-
-// Keyboard functions
-function keyDownHandler(e) {
-  if (e.key === "Right" || e.key === "ArrowRight") {
-    rightPressed = true;
-  } else if (e.key === "Left" || e.key === "ArrowLeft") {
-    leftPressed = true;
-  } else if (e.key === "Up" || e.key === "ArrowUp") {
-    upPressed = true;
-  } else if (e.key === "Down" || e.key === "ArrowDown") {
-    downPressed = true;
-  }
-}
-
-function keyUpHandler(e) {
-  if (e.key === "Right" || e.key === "ArrowRight") {
-    rightPressed = false;
-  } else if (e.key === "Left" || e.key === "ArrowLeft") {
-    leftPressed = false;
-  }
-}
+const xStart = 2;
+const yStart = 7;
+let direction = "right";
 
 //* Class definitions
-class Squares {
-  constructor(x, y, size, color) {
+class Square {
+  constructor(x, y, color) {
     this.x = x;
     this.y = y;
-    this.size = size;
     this.color = color;
   }
   draw() {
     ctx.beginPath();
     ctx.fillStyle = this.color;
-    ctx.rect(this.x, this.y, this.size, this.size);
+    ctx.rect(this.x * size, this.y * size, size, size);
     ctx.fill();
     ctx.closePath();
   }
 }
 
 class Snake {
-  constructor(x, y, dx, dy, length, color) {
-    this.x = x;
-    this.y = y;
-    this.dx = dx;
-    this.dy = dy;
-    this.length = length;
-    this.color = color;
+  constructor(segments) {
+    this.segments = segments;
   }
   draw() {
-    ctx.beginPath();
-    ctx.fillStyle = this.color;
-    ctx.rect(this.x, this.y, this.length * size, size);
-    ctx.fill();
-    ctx.closePath();
+    console.log(this.segments);
+    this.segments.forEach((segment) => {
+      segment.draw();
+    });
   }
-  update() {
-    if (this.dx === 1) {
-      this.x += size;
-    } else if (this.dx === -1) {
-      this.x -= size;
-    } else if (this.dy === 1) {
-      this.y += size;
-    } else if (this.dy === -1) {
-      this.y -= size;
+  move() {
+    const head = { ...this.segments[0] };
+    switch (direction) {
+      case "up":
+        head.y--;
+        break;
+      case "down":
+        head.y++;
+        break;
+      case "left":
+        head.x--;
+        break;
+      case "right":
+        head.x++;
+        break;
     }
+
+    this.segments.unshift(head);
+    this.segments.pop();
+
+    console.log(this.segments);
   }
 }
 
 //* Draw elements
 
-// Draw the squares
+// Draw background squares
 function drawSquares() {
   for (let i = 0; i < squaresRowCount; i++) {
     for (let j = 0; j < squaresRowCount; j++) {
       if ((i % 2 === 0 && j % 2 === 0) || (i % 2 !== 0 && j % 2 !== 0)) {
-        let square = new Squares(x, y, size, squareColor1);
+        let square = new Square(x, y, squareColor1);
         square.draw();
-        x += size;
+        x++;
       } else {
-        let square = new Squares(x, y, size, squareColor2);
+        let square = new Square(x, y, squareColor2);
         square.draw();
-        x += size;
+        x++;
       }
     }
     x = 0;
-    y += size;
+    y++;
   }
   y = 0;
 }
 
 drawSquares();
 
-// Draw the snake
-let snake = new Snake(0, 0, 1, 0, 3, "#bde0fe");
+console.log(xStart, yStart);
+// Draw snake
+const snake = new Snake([new Square(xStart, yStart, snakeColor)]);
+console.log(snake.segments);
+
 snake.draw();
 
-//* Animation
-
-function animate() {
-  requestAnimationFrame(animate);
-  now = Date.now();
-  delta = now - then;
-
-  if (delta > interval) {
-    // Update time
-    then = now - (delta % interval);
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawSquares();
-    snake.update();
-    snake.draw();
-    requestAnimationFrame(animate);
-  }
-}
-
-animate();
+setInterval(() => {
+  snake.move();
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawSquares();
+  snake.draw();
+}, 1000);
