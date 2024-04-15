86351e13d890361fa60f6f25fe38facb0c7a28b4
Keisy <keisy.nf@gmail.com>
2024-04-14 22:00:32 -0600

    Refactor snake movement logic, update grid variables, and add keyboard event handler in main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
 // Grid variables
-const gridCount = 16;
+const gridCount = 15;
 const size = canvas.width / gridCount;
 const squareColor1 = "#ffceda";
 const squareColor2 = "#febbcf";
@@ -13,25 +13,16 @@ const squareColor2 = "#febbcf";
 // Snake variables
 const snakeColor = "#bde0fe";
 let direction = "right";
-const x = 0;
-const y = 8;
+const x = 3;
+const y = Math.ceil(gridCount / 2) - 1;
 
-document.addEventListener("keydown", keyDownHandler, false);
+// Food variables
+const foodColor = "#e73668";
 
-function keyDownHandler(e) {
-  console.log(e.key);
-  if (e.key === "Enter") {
-    startGame();
-    document.removeEventListener("keydown", keyDownHandler, false);
-  }
-}
-
-function startGame() {
-  setInterval(() => {
-    snake.moveSnake();
-    draw();
-  }, 200);
-}
+// Game variables
+let gameStarted = false;
+let gameSpeedDelay = 200;
+let gameInterval;
 
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -66,15 +57,38 @@ function draw() {
 }
 
 class Square {
-  constructor(x, y, size, color) {
+  constructor(
+    x,
+    y,
+    size,
+    color,
+    border = false,
+    borderColor = "white",
+    borderSize = 2
+  ) {
     this.x = x;
     this.y = y;
     this.size = size;
     this.color = color;
+    this.border = border;
+    this.borderColor = borderColor;
+    this.borderSize = borderSize;
   }
   draw() {
+    ctx.beginPath();
     ctx.fillStyle = this.color;
     ctx.fillRect(this.x * this.size, this.y * this.size, this.size, this.size);
+    if (this.border) {
+      ctx.strokeStyle = "white"; // Set the color of the border
+      ctx.lineWidth = 2; // Set the width of the border
+      ctx.strokeRect(
+        this.x * this.size,
+        this.y * this.size,
+        this.size,
+        this.size
+      );
+    }
+    ctx.closePath();
   }
 }
 
@@ -93,7 +107,8 @@ class Snake {
       this.squares[0].x,
       this.squares[0].y,
       this.squares[0].size,
-      this.squares[0].color
+      this.squares[0].color,
+      true
     );
 
     switch (this.direction) {
@@ -112,17 +127,96 @@ class Snake {
     }
 
     this.squares.unshift(head);
-    this.squares.pop();
+
+    if (head.x === food.x && head.y === food.y) {
+      food.x = random(0, gridCount - 1);
+      food.y = random(0, gridCount - 1);
+      clearInterval(gameInterval);
+      gameInterval = setInterval(() => {
+        snake.moveSnake();
+        snake.collisionDetection();
+        draw();
+      }, gameSpeedDelay);
+    } else {
+      this.squares.pop();
+    }
+  }
+
+  collisionDetection() {
+    const head = this.squares[0];
+    if (
+      head.x < 0 ||
+      head.x >= gridCount ||
+      head.y < 0 ||
+      head.y >= gridCount
+    ) {
+      restartGame();
+    } else {
+      for (let i = 1; i < this.squares.length; i++) {
+        if (head.x === this.squares[i].x && head.y === this.squares[i].y) {
+          restartGame();
+        }
+      }
+    }
   }
 }
 
-const snake = new Snake([new Square(x, y, size, snakeColor)], direction);
+const snake = new Snake([new Square(x, y, size, snakeColor, true)], direction);
 
 const food = new Square(
   random(0, gridCount - 1),
   random(0, gridCount - 1),
   size,
-  "red"
+  foodColor,
+  true
 );
 
 draw();
+
+document.addEventListener("keydown", keyDownHandler, false);
+
+function keyDownHandler(e) {
+  if (!gameStarted && e.key === "Enter") {
+    startGame();
+  } else {
+    if (
+      (e.key === "Right" || e.key === "ArrowRight") &&
+      snake.direction !== "left"
+    ) {
+      snake.direction = "right";
+    } else if (
+      (e.key === "Left" || e.key === "ArrowLeft") &&
+      snake.direction !== "right"
+    ) {
+      snake.direction = "left";
+    } else if (
+      (e.key === "Up" || e.key === "ArrowUp") &&
+      snake.direction !== "down"
+    ) {
+      snake.direction = "up";
+    } else if (
+      (e.key === "Down" || e.key === "ArrowDown") &&
+      snake.direction !== "up"
+    ) {
+      snake.direction = "down";
+    }
+  }
+}
+
+function startGame() {
+  gameStarted = true;
+  gameInterval = setInterval(() => {
+    snake.moveSnake();
+    snake.collisionDetection();
+    draw();
+  }, gameSpeedDelay);
+}
+
+function restartGame() {
+  clearInterval(gameInterval);
+  snake.squares = [new Square(x, y, size, snakeColor, true)];
+  snake.direction = "right";
+  gameSpeedDelay = 200;
+  gameStarted = false;
+  draw();
+}
